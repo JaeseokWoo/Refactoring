04d0efa1239514576b5d51e26e2802e827a8503a
JaeseokWoo <JaeseokW@gmail.com>
2022-03-18 19:23:19 +0900

    REFACT: 반복문 쪼개기

--- ch1/statement.js
+++ ch1/statement.js
@@ -8,11 +8,13 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
 
-        volumeCredits += volumeCreditsFor(perf);
-
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
+    }
+
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
