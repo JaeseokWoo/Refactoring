b21a74e33daff58979de830d0cd784e297bbf8ed
JaeseokWoo <JaeseokW@gmail.com>
2022-03-18 19:01:49 +0900

    REFACT: amoutFor 함수 추출

--- ch1/statement.js
+++ ch1/statement.js
@@ -9,6 +9,20 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
+        let thisAmount = amountFor(perf, play);
+
+        volumeCredits += Math.max(perf.audience - 30, 0);
+
+        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `총액: ${format(totalAmount/100)}\n`;
+    result += `적립 포인트: ${volumeCredits}점\n`;
+    return result;
+
+    function amountFor(perf, play) {
         let thisAmount = 0;
 
         switch (play.type) {
@@ -28,17 +42,8 @@ function statement(invoice, plays) {
             default:
                 throw new Error(`알 수 없는 장르: ${play.type}`);
         }
-
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        return thisAmount;
     }
-    result += `총액: ${format(totalAmount/100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
-    return result;
 }
 
 module.exports = statement;
