cc6557a7186b2e12e1ea29ee2fb91897160408b7
JaeseokWoo <JaeseokW@gmail.com>
2022-03-18 19:07:14 +0900

    REFACT: 임시 변수를 질의 함수로 바꾸기

--- ch1/statement.js
+++ ch1/statement.js
@@ -8,7 +8,7 @@ function statement(invoice, plays) {
     const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
 
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -22,22 +22,26 @@ function statement(invoice, plays) {
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 
-    function amountFor(perf, play) {
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
+    function amountFor(aPerformance, play) {
         let result = 0;
 
         switch (play.type) {
             case "tragedy":
                 result = 40000;
-                if (perf.audience > 30) {
-                    result += 1000 * (perf.audience - 30);
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
                 }
                 break;
             case "comedy":
                 result = 30000;
-                if (perf.audience > 20) {
-                    result += 10000 + 500 * (perf.audience - 20);
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
                 }
-                result += 300 * perf.audience;
+                result += 300 * aPerformance.audience;
                 break;
             default:
                 throw new Error(`알 수 없는 장르: ${play.type}`);
