3b729c160cc310580c40ca3a693e435de43149bc
JaeseokWoo <JaeseokW@gmail.com>
2022-03-18 19:34:47 +0900

    REFACT: volumeCredits 계산 코드 함수로 추출

--- ch1/statement.js
+++ ch1/statement.js
@@ -3,7 +3,6 @@ const invoices = require('./invoices');
 
 function statement(invoice, plays) {
     let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
     for (let perf of invoice.performances) {
@@ -11,13 +10,18 @@ function statement(invoice, plays) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
-    }
+    let volumeCredits = totalVolumeCredits();
 
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += volumeCreditsFor(perf);
+        }
+    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 })
