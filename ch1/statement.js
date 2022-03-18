de2a1d23d09504799b93fcb583c6b96b6640c2f1
JaeseokWoo <JaeseokW@gmail.com>
2022-03-18 20:05:10 +0900

    REFACT: 적립 포인트 계산 부분 옮김

--- ch1/statement.js
+++ ch1/statement.js
@@ -10,7 +10,8 @@ function statement(invoice, plays) {
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
         result.play = playFor(result);
-        result.amout = amountFor(result);
+        result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
 
@@ -40,6 +41,15 @@ function statement(invoice, plays) {
         }
         return result;
     }
+
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if("comedy" === aPerformance.play.type) {
+            result += Math.floor(aPerformance.audience / 5);
+        }
+        return result;
+    }
 }
 
 function renderPlainText(data, plays) {
@@ -65,7 +75,7 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits() {
         let result = 0;
         for (let perf of data.performances) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
