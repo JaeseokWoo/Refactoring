792353cc97e4060b7882bca49d7a7d944b81ac43
JaeseokWoo <JaeseokW@gmail.com>
2022-03-18 20:57:01 +0900

    REFACT: TragedyCalculator amout 오버라이드

--- ch1/createStatementData.js
+++ ch1/createStatementData.js
@@ -9,11 +9,7 @@ class PerformanceCalculator {
 
         switch (this.play.type) {
             case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
+                throw '오류 발생';
             case "comedy":
                 result = 30000;
                 if (this.performance.audience > 20) {
@@ -37,8 +33,26 @@ class PerformanceCalculator {
     }
 }
 
+class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }
+}
+class ComedyCalculator extends PerformanceCalculator {
+
+}
+
 function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch (aPlay.type) {
+        case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+        case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+        default:
+            throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+    }
 }
 
 function createStatementData(invoice, plays) {
