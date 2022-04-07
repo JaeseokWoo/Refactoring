70eab0fbe6b86d310b983d5eeb7aa61a032a1201
JaeseokWoo <JaeseokW@gmail.com>
2022-04-07 21:33:49 +0900

    REFACT: 값 클래스 적용

--- ch7/7.3/Order.js
+++ ch7/7.3/Order.js
@@ -1,12 +1,12 @@
 class Order {
   constructor(data) {
-    this.priority = data.priority;
+    this._priority = new Priority(data.priority);
   }
 
   get priority() {
-    return this.priority;
+    return this._priority.toString();
   }
   set priority(aString) {
-    this.priority = aString;
+    this._priority = new Priority(aString);
   }
 }
