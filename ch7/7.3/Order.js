30e10d7f9056ab6d699ac9ec1d0dacbd108911d1
JaeseokWoo <JaeseokW@gmail.com>
2022-04-07 21:30:08 +0900

    REFACT: 변수 캡슐화

--- ch7/7.3/Order.js
+++ ch7/7.3/Order.js
@@ -2,4 +2,11 @@ class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  get priority() {
+    return this.priority;
+  }
+  set priority(aString) {
+    this.priority = aString;
+  }
 }
