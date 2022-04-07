9ab45e819ba345f650b86f1f741e258ddae327e6
JaeseokWoo <JaeseokW@gmail.com>
2022-04-07 18:53:17 +0900

    REFACT: 세터 제거하기

--- ch7/7.2/Person.js
+++ ch7/7.2/Person.js
@@ -7,9 +7,20 @@ class Person {
     return this._name;
   }
   get courses() {
-    return this._courses;
+    return this._courses.slice();
   }
-  set courses(aList) {
-    this._courses = aList;
+
+  addCourse(aCourse) {
+    this._courses.push(aCourse);
+  }
+  removeCourse(
+    aCourse,
+    fnIfAbsent = () => {
+      throw new RangeError();
+    }
+  ) {
+    const index = this._courses.indexOf(aCourse);
+    if (index === -1) fnIfAbsent();
+    else this._courses.splice(index, 1);
   }
 }
